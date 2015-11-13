a4159290bb954b42588278b48811dd6b2c0cf6b2
Heybye44 <Heybye44@gmail.com>
2015-11-12 20:57:36 -0500

    Revamped evrything; Don't worry not dead. XD

---
+++ js/links.js
@@ -0,0 +1,21 @@
+function gotoPE(){
+	window.open("typesofenergy/pe.html");
+}
+
+function gotoKE(){
+	window.open("typesofenergy/ke.html");
+}
+function gotoPEfrom(){
+	window.open("../typesofenergy/pe.html");
+}
+
+function gotoKEfrom(){
+	window.open("../typesofenergy/ke.html");
+}
+function goBack(){
+	window.close();
+}
+function gotoGitPage(){
+	window.open("http://heybye44.github.io");
+}
+new WOW().init();
