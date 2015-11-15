8b1d2f8441dca86d7aca89bf2fa9dd2fcc85760d
Heybye44 <Heybye44@gmail.com>
2015-11-14 21:13:18 -0500

    Added a school project that needs hosting.

---
+++ sites/energy/js/links.js
@@ -0,0 +1,28 @@
+function gotoPE(){
+	window.open("typesofenergy/pe.html");
+}
+
+function gotoKE(){
+	window.open("typesofenergy/ke.html");
+}
+function gotoPEfrom(){
+	window.open("../typesofenergy/pe.html");
+}
+
+function gotoKEfrom(){
+	window.open("../typesofenergy/ke.html");
+}
+function goBack(){
+	window.close();
+}
+function gotoGitPage(){
+	window.open("http://heybye44.github.io");
+}
+function gotoEnd(){
+	window.open("../typesofenergy/end.html")
+}
+new WOW().init();
+
+window.onublur = function(){
+	
+}
